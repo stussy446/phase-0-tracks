faee12f901456769fdd8281f1078e5b099bbace4
Steve Rector <steverector27@gmail.com>
2017-04-21 00:59:51 -0700

    All conditions for challenge pass

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,20 +1,31 @@
                      # Vampire interview 
 # ask the interviewee questions 
+	# Name?
+	# age? Date of birth?
+	# garlic bread?
+	# insurance?
 
 # if the employee gets their age right and wants garlic bread or insurance, 
-#   they are probably not a vampire
+  # they are probably not a vampire
 # if the employee got age wrong and says no to garlic bread or insurance, 
-# 	they probably are a vampire
+ 	# they probably are a vampire
 # if they got age wrong and said no to bread and insurance,
-# 	they almost certainly are a vampire 
+ 	# they almost certainly are a vampire 
 # If there name is "Drake Cula" or "Tu Fang", they are definitely a vamp.
 # otherwise, the results are inconclusive 
 
 # ask how many employees they want to process, and have each take the survey
+# add a section that allows employees to list their allergies until they
+# are done, or until they say the dreaded sunshine. If they do, the are probably a vampire
+
+# Finish with a plea for friendship 
+
 puts "How many employees will be processed?"
 employees = gets.chomp.to_i
+# Employee loop 
 for i in 1..employees
 	vampire_name = false
+	vampire_allergy = false
 	puts "What is your name?"
 	name = gets.chomp 
 	if name == "Drake Cula" || name == "Tu Fang"
@@ -29,6 +40,7 @@ for i in 1..employees
 	puts "Would you like to enroll in the company's health insurance policy?"
 	insurance = gets.chomp
 	puts "Please let us know any allergies you may have, one at a time."
+	# Allergy loop
 	loop {
 		puts "allergy name --"
 		allergy = gets.chomp 
@@ -36,16 +48,19 @@ for i in 1..employees
 			break
 		elsif allergy == "sunshine"
 			puts "oh no..."
+			vampire_allergy = true 
 			break
 		end 
 	}
 	# Conditional logic
 	if vampire_name 
 		puts "Definitely a vampire."
-	elsif (2017 - age == year) && (garlic_bread == "y" || insurance == "y")
+	elsif vampire_allergy
+		puts "Probably a vampire."
+	elsif (2017 - age == year) && (garlic_bread == "y" || insurance == "y") 
 		puts "Probably not a vampire."
 	elsif (2017 - age != year) && (garlic_bread == "n" || insurance == "n" )
-		if (2017 - age != year) && (garlic_bread == "n") && (insurance == "n")
+		if (2017 - age != year) && (garlic_bread == "n") && (insurance == "n") 
 			puts "Almost certainly a vampire."
 		else
 			puts "Probably a vampire."
@@ -54,3 +69,4 @@ for i in 1..employees
 		puts "Results inconclusive."	
 	end
 end 
+puts "Actually, never mind! What do these questions have to do with anything? Let's\n all be friends."
