b9f26ef483c604164f9ef4f1a48e4729ef04af89
Steve Rector <steverector27@gmail.com>
2017-04-25 02:03:11 -0700

    Add working logic, DRY code

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -9,32 +9,42 @@ age = gets.chomp.to_i
 puts "How many children do you have?"
 children = gets.chomp.to_i
 puts "What is your preferred decor theme?"
-decor_theme = gets.chomp 
+decor = gets.chomp 
+puts "Are you a new customer?(y/n)"
+first = gets[0] == "y"
 
-# Create a hash and add in the customer's inputs 
+# Create a hash and add in the customer's inputs.
+# Print the hash (client_info) 
+
 client_info = {
   name: name, 
   age: age, 
   children: children, 
-  decor_theme: decor_theme
+  decor: decor,
+  first: first
 }
 
-print client_info
+puts client_info
 
 # Give user the option to update a key. Skip if user says "none"
 # If the designer enters "decor_theme"(for example), ask for a new value
 # and update the :decor_theme
-
-puts "Would you like to update one of the options?"
+puts "Please select any option that you would like to update at this time \n (if satisfied, enter \"none\")"
 choice = gets.chomp.to_sym
-if choice == "none"
-  puts "Ok, moving on."
-elsif choice == :name 
-  puts "Ok please give me a new value for #{choice}"
-  new_value = gets.chomp
+if client_info.has_key?(choice)
+  puts "Ok, enter the change for #{choice}"
+  new_value = gets.chomp 
   client_info[choice] = new_value
-end
-print client_info
-
+elsif choice == :none
+  puts "Glad you are feeling good about this."
+else 
+  puts "" 
+end 
 
 # Print the latest version of the hash
+puts "Great, Lets finish up..."
+puts client_info
+
+
+
+
