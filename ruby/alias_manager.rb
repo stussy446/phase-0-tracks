e0442ce828624f895250fb1b65d0eb9f259eb4e4
Steve Rector <steverector27@gmail.com>
2017-04-26 16:47:57 -0700

    Swapping first and last name correctly

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# SPY ALGORITHM: Write a method that make's a spy's name fake
+
+def fake_name(fullname)
+  #swap the first and last name.
+  reverse_name = fullname.split(' ').reverse.join(' ')
+  # Change the vowels to the next vowel and consonantts to the next consant
+end 
+
+puts fake_name("Steve Rector")
