6bac258e48aedd9093fa9302e1d1d0c2ead9b806
Steve Rector <steverector27@gmail.com>
2017-04-26 15:19:25 -0700

    Working method, each, and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,24 @@ def status
 end 
 
 status{ |word1, word2| puts "#{word1}, way more #{word2} status message!!"}
+
+sports = ["Hockey", "Baseball", "Basketball", "Football"]
+game_of_thrones = {
+  queen: "Cersei",
+  knight: "Jamie",
+  bastard: "Jon Snow",
+  no_name: "Aria Stark"
+}
+
+sports.map!{|sport| 
+  sport + " is awesome!"
+}
+
+
+game_of_thrones.each{|title, character| 
+  puts "#{game_of_thrones[title]} is an important part of Game of Thrones."
+}
+
+sports.each{|sport|
+  puts "#{sport} is my favorite sport!"
+}
