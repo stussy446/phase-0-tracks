e152f93063481feb3184c34f7ad3cc92efc60d0b
Steve Rector <steverector27@gmail.com>
2017-04-26 19:43:01 -0700

    Basic UI complete

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,15 @@
 # SPY ALGORITHM: Write a method that make's a spy's name fake
+# swap the first/last name and then swap letters
 # input example: "Steve Rector", "Danielle "Rector"
 # output example: Sidvus Tviwi , "Sidvus Fepoimmi"
 
 def fake_name(fullname)
   #swap the first and last name.
+  
   reverse_name = fullname.downcase.split(' ').reverse.join(' ')
   
   # Change the vowels to the next vowel and consonants to the next consant
-  # "Steve Rector" should change the "e"s to "i"s and the "o" to u
+
   letters = reverse_name.split('')
   vowels = ["a", "e", "i", "o", "u"]
   consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
@@ -24,8 +26,29 @@ def fake_name(fullname)
   letters.join('').capitalize
 end 
 
-puts fake_name("Steve Rector")
-puts fake_name("Danielle Rector")
+#puts fake_name("Steve Rector")
+#puts fake_name("Danielle Rector")
+
+# USER INTERFACE
+
+puts "Enter a name that would would like an alias for."
+puts "If your sneakiness is complete, type \"quit\""
+loop do
+  puts "Enter name here--"
+  old_name = gets.chomp
+  if old_name == "quit"
+    break
+  else
+    new_name = fake_name(old_name)
+    puts "Your new name is...#{new_name}"
+  end
+end
+puts "Thank you for using the Fake Naminator!"
+  
+
+
+
+
 
 
 
