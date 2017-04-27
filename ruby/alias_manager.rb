147d6acd558b85170de6e86e02be05999b2606d4
Steve Rector <steverector27@gmail.com>
2017-04-26 17:57:56 -0700

    Release 0 complete

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,11 @@
 # SPY ALGORITHM: Write a method that make's a spy's name fake
-# input example: "Steve Rector"
-# output example: Sidvustviwi
+# input example: "Steve Rector", "Danielle "Rector"
+# output example: Sidvus Tviwi , "Sidvus Fepoimmi"
 
 def fake_name(fullname)
   #swap the first and last name.
   reverse_name = fullname.downcase.split(' ').reverse.join(' ')
+  
   # Change the vowels to the next vowel and consonants to the next consant
   # "Steve Rector" should change the "e"s to "i"s and the "o" to u
   letters = reverse_name.split('')
@@ -15,13 +16,16 @@ def fake_name(fullname)
       letter = vowels[vowels.index(letter) + 1]
     elsif consanants.include?(letter)
       letter = consanants[consanants.index(letter) + 1]
+    else
+      letter = " "
     end
   }
   
   letters.join('').capitalize
 end 
 
-print fake_name("Steve Rector")
+puts fake_name("Steve Rector")
+puts fake_name("Danielle Rector")
 
 
 
