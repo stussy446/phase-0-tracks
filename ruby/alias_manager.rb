8c74ed4beefc52aec00f08746d1c54babf92c389
Steve Rector <steverector27@gmail.com>
2017-04-26 17:20:38 -0700

    Succesfully switching vowels to next vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,6 +4,16 @@ def fake_name(fullname)
   #swap the first and last name.
   reverse_name = fullname.split(' ').reverse.join(' ')
   # Change the vowels to the next vowel and consonantts to the next consant
+  # "Steve Rector" should change the "e"s to "i"s and the "o" to u
+  letters = reverse_name.split('')
+  vowels = ["a", "e", "i", "o", "u"]
+  letters.map!{|letter|
+    if vowels.include?(letter)
+      letter = vowels[vowels.index(letter) + 1]
+    end
+  }
+
+
 end 
 
-puts fake_name("Steve Rector")
+print fake_name("Steve Rector")
