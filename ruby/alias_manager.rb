6691f50f3e67d1fb7d5d2e685ec425b090844227
Steve Rector <steverector27@gmail.com>
2017-04-26 22:52:37 -0700

    Complete up to release 2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,31 +5,41 @@
 
 def fake_name(fullname)
   #swap the first and last name.
+  # make all letters lowercase
   
   reverse_name = fullname.downcase.split(' ').reverse.join(' ')
   
-  # Change the vowels to the next vowel and consonants to the next consant
+  # Change the vowels to the next vowel and consonants to the next consanant
 
   letters = reverse_name.split('')
   vowels = ["a", "e", "i", "o", "u"]
-  consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y",]
   letters.map!{|letter|
     if vowels.include?(letter)
       letter = vowels[vowels.index(letter) + 1]
     elsif consanants.include?(letter)
       letter = consanants[consanants.index(letter) + 1]
+    elsif letter == "z" 
+      letter = "a"
     else
       letter = " "
     end
   }
   
-  letters.join('').capitalize
+  # join the letters array to create a string
+  # split that string by the space and capitalize each element in the string
+  # then join that string back together for the return.
+  
+  fake_name = letters.join('')
+  fake_name.split(' ').map!{|word| word.capitalize}.join(' ')
+
 end 
 
-#puts fake_name("Steve Rector")
-#puts fake_name("Danielle Rector")
-
 # USER INTERFACE
+
+#create a loop that uses the fake_name method to create aliases
+# if the user inputs "quit", end the loop
+# otherwise, create the new name and store it in a hash.
 
 puts "Enter a name that would would like an alias for."
 puts "If your sneakiness is complete, type \"quit\""
