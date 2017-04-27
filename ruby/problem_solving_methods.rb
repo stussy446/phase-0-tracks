aa9af9fea0b1d7295dc1b3daefb40af3f0a83cd5
Steve Rector <steverector27@gmail.com>
2017-04-27 15:50:47 -0700

    Release 1 complete

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -4,18 +4,31 @@
 # Do not use .index
 
 def search_array(array, number)
-  array.each{|item| 
-    if item == number
+    if array.include?(number)
       number = (0..array.length - 1).select{|i| array[i] == number} 
-      
     else
       nil
     end
-  }
+  
   number
 end
 
-
 arr = [1,2,3,4,5]
-puts search_array(arr, 5 )
+puts search_array(arr, 2 )
+
+#Define a method that takes one integer argument
+#Method should generate and return an array of the terms of the Fibonacci sequence
+
+def fibonacci(int)
+  arr = [0, 1, 1]
+  index = 2
+  int.times{|i|
+    i = arr[index] + arr[index - 1]
+    arr << i 
+    index +=1
+  }
+ arr 
+end 
+
+puts fibonacci(5)
 
