7e13c81c7891c44f62c6a90d04d8dd8fd7e1b834
Steve Rector <steverector27@gmail.com>
2017-04-26 23:13:13 -0700

    Conditions all working

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,20 +1,22 @@
 # SPY ALGORITHM: Write a method that make's a spy's name fake
-# swap the first/last name and then swap letters
-# input example: "Steve Rector", "Danielle "Rector"
-# output example: Sidvus Tviwi , "Sidvus Fepoimmi"
+# Cwap the first/last name and then swap letters
+# Input example: "Steve Rector", "Danielle "Rector"
+# Output example: Sidvus Tviwi , "Sidvus Fepoimmi"
 
 def fake_name(fullname)
-  #swap the first and last name.
-  # make all letters lowercase
+  
+  # Swap the first and last name.
+  # Make all letters lowercase
   
   reverse_name = fullname.downcase.split(' ').reverse.join(' ')
   
   # Change the vowels to the next vowel and consonants to the next consanant
+  # Create a condition for "z" to turn into "a" and for everything else to be a space
 
   letters = reverse_name.split('')
   vowels = ["a", "e", "i", "o", "u"]
   consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y",]
-  letters.map!{|letter|
+  letters.map! { |letter|
     if vowels.include?(letter)
       letter = vowels[vowels.index(letter) + 1]
     elsif consanants.include?(letter)
@@ -26,8 +28,8 @@ def fake_name(fullname)
     end
   }
   
-  # join the letters array to create a string
-  # split that string by the space and capitalize each element in the string
+  # Join the letters array to create a string
+  # Split that string by the space and capitalize each element in the string
   # then join that string back together for the return.
   
   fake_name = letters.join('')
@@ -37,12 +39,13 @@ end
 
 # USER INTERFACE
 
-#create a loop that uses the fake_name method to create aliases
-# if the user inputs "quit", end the loop
-# otherwise, create the new name and store it in a hash.
+# Create a loop that uses the fake_name method to create aliases
+# If the user inputs "quit", end the loop
+# Otherwise, create the new name and store it in a hash.
 
 puts "Enter a name that would would like an alias for."
 puts "If your sneakiness is complete, type \"quit\""
+aliases = {}
 loop do
   puts "Enter name here--"
   old_name = gets.chomp
@@ -51,10 +54,17 @@ loop do
   else
     new_name = fake_name(old_name)
     puts "Your new name is...#{new_name}"
+    aliases[old_name] = new_name
   end
 end
-puts "Thank you for using the Fake Naminator!"
-  
+
+# Iterate through aliases and print a sentence saying who each alias is.
+# Example input: Felicia Torres
+# Example output: Vussit Gimodoe is actually Felicia Torres!!
+
+aliases.each { |person, fake_name| 
+  puts "#{fake_name} is acually #{person}!!"
+}
 
 
 
