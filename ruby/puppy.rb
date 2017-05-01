1f00771465bac62746d6bd905027876080f46ad7
Steve Rector <steverector27@gmail.com>
2017-05-01 15:21:18 -0700

    Add puppy design for release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# Animal --------------
+  # Puppy
+
+# Characteristics -------------
+  # Eyes: baby 
+  # Energy: High
+  # Name: varies 
+  # fur color: varies 
+  # Friendly: yes 
+  # Hungry: yes 
+
+# Behavior ----------------
+  # Bark 
+  # Eat 
+  # Play 
+  # Sleep  
