e02c0d5ee8d7abb352f6b5f38fcaebbca42db6f6
Steve Rector <steverector27@gmail.com>
2017-05-02 02:32:57 -0700

    Finish up to get mad method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,7 +17,29 @@ class Santa
 
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
-  end 
+  end
+
+  # Celebrate Birthday method 
+  # add one to age each time called 
+
+  def celebrate_birthday()
+    @age += 1 
+  end  
+
+  # get mad at reindeeer method 
+  # takes a name, if the name matches a name in @reindeer_ranking, that
+  # reindeer goes to last in the list 
+
+  def get_mad_at(reindeer_name) 
+    if @reindeer_ranking.include?(reindeer_name)
+     bad_reindeer = @reindeer_ranking.index(reindeer_name)
+     @reindeer_ranking.insert(-1, reindeer_name)
+     @reindeer_ranking.delete_at(bad_reindeer)
+    end
+  end
+
+
+
 
 end
 
@@ -30,9 +52,15 @@ end
 # st_nick.speak 
 # st_nick.eat_milk_and_cookies("Oatmeal Chocolate Chip")
 
-santas = []
-genders = ["man", "woman", "Bisexual", "Pisexual"]
-ethnicities = ["White", "Black", "Asian", "Middle-Eastern", "Native American"]
-ethnicities.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
-end 
+# santas = []
+# genders = ["man", "woman", "Bisexual", "Pisexual"]
+# ethnicities = ["White", "Black", "Asian", "Middle-Eastern", "Native American"]
+# ethnicities.length.times do |i|
+  # santas << Santa.new(genders[i], ethnicities[i])
+# end
+
+nick = Santa.new("Nick", "White")
+nick.celebrate_birthday
+nick.celebrate_birthday
+nick.get_mad_at("Rudolph")
+ 
