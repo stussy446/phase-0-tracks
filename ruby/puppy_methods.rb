5cd2023be3803d2426d9212783067134c084e909
Steve Rector <steverector27@gmail.com>
2017-05-03 12:40:26 -0700

    Finish part 1 of Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    number.times { puts "Woof!"}
+  end 
+
+  def roll_over()
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    7 * human_years
+  end
+
+  def play_dead()
+    puts "The puppy lays there motionless..."
+  end   
 end
+
+# TEST CODE 
+
+dog = Puppy.new
+dog.fetch("ball")
+dog.speak(3)
+dog.roll_over
+puts dog.dog_years(7)
+dog.play_dead 
 
