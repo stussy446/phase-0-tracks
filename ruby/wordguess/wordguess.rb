a7493c8fd3fb518886e30f06fa8e755f850dc0d8
Steve Rector <steverector27@gmail.com>
2017-05-05 00:03:21 -0700

    compare words method working with passing tests

--- ruby/wordguess/wordguess.rb
+++ ruby/wordguess/wordguess.rb
@@ -14,24 +14,40 @@
   # The user should get a congratulate message if they win and a insulting message if they don't         
 
 class WordGuess
-  attr_reader :guess_count, :already_guessed
+  attr_reader :guess_count, :already_guessed, :game_board
   
   def initialize(secret_word)
-    puts "initializing game..."
     @already_guessed = []
     @secret_word = secret_word
+    @game_board = ("_" * @secret_word.length).split('')
     @guess_count = 0 
     @is_over = false 
   end 
 
   def guess(word)
-    if already_guessed.include?(word)
+    if @already_guessed.include?(word)
       "Word already guessed, does not count!"
     else 
       @guess_count += 1 
-      already_guessed << word  
+      @already_guessed << word  
       word
     end 
   end
+
+  def compare_words(word)
+    secret_word_letters = @secret_word.split('')
+    guess_word_letters = word.split('')
+    guess_word_letters.each do |letter|
+      if secret_word_letters.include?(letter)
+        correct_letter = secret_word_letters.index(letter)
+        @game_board[correct_letter] = letter 
+      end 
+    end
+    @game_board  
+  end 
+
+    
+
+
 end
 
--- ruby/wordguess/wordguess_spec.rb
+++ ruby/wordguess/wordguess_spec.rb
@@ -5,6 +5,7 @@ describe WordGuess do
 
   it "lets the user read the guess count" do 
     expect(game.guess_count).to eq 0
+    expect(game.game_board). to eq ["_", "_", "_", "_", "_", "_", "_"]
   end
   it "takes a guessed word and adds 1 to guess_count" do 
     expect(game.guess("taco")).to eq "taco"
@@ -12,4 +13,10 @@ describe WordGuess do
     expect(game.guess_count).to eq 1
     expect(game.already_guessed).to eq ["taco"]
   end
+
+  it "compares a guess word with the secret word and returns the updated game board." do 
+    expect(game.compare_words("use")).to eq ["u", "_", "_", "_", "_", "_", "_"]
+    expect(game.compare_words("need")).to eq ["u", "n", "_", "_", "_", "_", "_"]
+
+  end 
 end
