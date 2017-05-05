2ba563a57e91a18f8dc8b1cfefb86389163ca756
Steve Rector <steverector27@gmail.com>
2017-05-04 23:31:23 -0700

    Initialize method working properly

--- ruby/wordguess/wordguess.rb
+++ ruby/wordguess/wordguess.rb
@@ -1,2 +1,27 @@
+ # Word Guessing Game
+
+# WordGuess Class 
+
+  # method that lets the user enter a word which will be the secret     
+  # word 
+
+  # the second user should be able to guess words a certain amount of  # times, until the word is guessed or the maximum guesses have       
+  # happened
+  # if the user has already guessed the word, it won't count as a guess
+
+  #The guessing player should recieve continuous feedback on the state of the word 
+
+  # The user should get a congratulate message if they win and a insulting message if they don't         
+
 class WordGuess
+  attr_reader :guess_count
+  
+  def initialize(secret_word)
+    puts "initializing game..."
+    @secret_word = secret_word
+    @guess_count = 0 
+    @is_over = false 
+  end 
+
 end
+
--- ruby/wordguess/wordguess_spec.rb
+++ ruby/wordguess/wordguess_spec.rb
@@ -1,4 +1,9 @@
 require_relative "wordguess"
 
 describe WordGuess do
+  let (:game) {WordGuess.new("unicorn")}
+
+  it "lets the user read the guess count" do 
+    expect(game.guess_count).to eq 0
+  end
 end
