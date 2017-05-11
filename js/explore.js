fe15c4a954f69de3c15d2985cdea4d2a604909eb
Steve Rector <steverector27@gmail.com>
2017-05-11 03:06:59 -0700

    Add functioning reverse method

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,19 @@
 // reverse function that takes a string and reverses it 
 	// example input: "hello"
 	// example output : "olleh"
-	// take the word and save each element into a variable 
-	// pass that variable into a string 
-	// return the string 
+	// create backwards variable that is an empty string 
+	// loop through one less than the length of the word, subtract each iteration 
+		// each iteration, add the corresponding letter of word[i] to
+		// backwards variable 
+	// return backwards variable, which should hold the reverse word
 
+function reverse(word){
+	backwards = "";
+	for(i = word.length - 1; i >= 0; i--){
+		backwards = backwards + word[i];
+	}
+	return backwards;
+}
+
+console.log(reverse("hello"))
+console.log(reverse("steve"))
