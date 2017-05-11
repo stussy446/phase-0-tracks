0acdea402f65f615f62dc7f22619c88db9032e62
Steve Rector <steverector27@gmail.com>
2017-05-11 02:51:58 -0700

    Add explore.js file

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// reverse function that takes a string and reverses it 
+	// example input: "hello"
+	// example output : "olleh"
+	// take the word and save each element into a variable 
+	// pass that variable into a string 
+	// return the string 
+
