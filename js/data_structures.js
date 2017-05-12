3630f8fe79b82c5f15397239372116a7b94d3182
Steve Rector <steverector27@gmail.com>
2017-05-12 13:35:53 -0700

    Complete object iteration release

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,5 +3,16 @@ var names = ["Ed", "Joe", "Ali", "Ruby"];
 colors.push("purple");
 names.push("Buck");
 
+// keys should be names, values should be color 
+var horses = {};
+for(i = 0; i < colors.length; i++){
+	horses[names[i]] = colors[i];
+}
+
+
+
+// DRIVER CODE 
+
 console.log(colors);
 console.log(names);
+console.log(horses)
