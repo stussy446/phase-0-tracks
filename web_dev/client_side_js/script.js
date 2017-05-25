21bea590ac321e8478c1e94cd216f2ab03f14c94
Steve Rector <steverector27@gmail.com>
2017-05-24 18:45:38 -0700

    Add event listenters, finish challenge

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,3 +6,23 @@ var h2Text = document.createTextNode("This is our added header!");
 newHeading.appendChild(h2Text);
 
 header_border.appendChild(newHeading);
+
+function addBlueBorder(event){
+  event.target.style.border = "2px solid blue";
+}
+
+function addPurpleBackground(event){
+  event.target.style.background = "purple";
+}
+
+var ul = document.getElementById("list");
+ul.addEventListener("click", addBlueBorder);
+
+var list = document.getElementsByTagName('li');
+for(i = 0; i < list.length; i++){
+  list[i].addEventListener("click", addPurpleBackground);
+}
+
+var paragraph = document.getElementsByTagName('p')[0];
+paragraph.addEventListener("click", addPurpleBackground);
+
