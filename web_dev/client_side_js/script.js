3938e0706ad8ca45cd304d675b3ae92861bf8338
Steve Rector <steverector27@gmail.com>
2017-05-24 18:27:09 -0700

    Add h2 to our html

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,2 +1,8 @@
 var header_border = document.getElementById("header");
 header_border.style.border = "3px solid red";
+
+var newHeading = document.createElement('h2');
+var h2Text = document.createTextNode("This is our added header!");
+newHeading.appendChild(h2Text);
+
+header_border.appendChild(newHeading);
