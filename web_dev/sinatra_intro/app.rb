7c104c6bedb36de0e3b2609b4728ac039a1f6a61
Steve Rector <steverector27@gmail.com>
2017-05-26 01:03:24 -0700

    Add address and add routes, basic great_job route but not done

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,35 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET /contact route that displays an address 
+
+get '/contact' do 
+  "address: 3260 De Anza Place"
+end
+
+#write a /great_job route that can take a person's name as a query parameter
+# and say "Good Jon #{persons_name}!"
+# If the name is not present, the route simply says "gd job!"
+
+get '/great_job' do 
+  "Good job"
+end
+
+# A route that uses route parameters to add two numbers and respond with 
+# the result. The data types are tricky here -- when will the data need to
+# be (or arrive as) a string?
+
+get '/add/:number_1/plus/:number_2' do 
+  number_1 = params[:number_1].to_i
+  number_2 = params[:number_2].to_i
+  answer = number_1 + number_2
+  answer.to_s 
+end
+
+
+
+
+
+
+
